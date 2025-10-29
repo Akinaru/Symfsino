b13dd5e4cd81a8c2a10f53b2dc06d0797e0500ad
Akinaru <maxime.utchimata@gmail.com>
2025-10-29 14:20:48 +0100

    ADD [global] Fix parcours

--- src/Controller/Admin/AdminUtilisateurController.php
+++ src/Controller/Admin/AdminUtilisateurController.php
@@ -3,7 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Utilisateur;
-use App\Form\Utilisateur1Type;
+use App\Form\UtilisateurType;
 use App\Repository\UtilisateurRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -26,7 +26,7 @@ final class AdminUtilisateurController extends AbstractController
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $utilisateur = new Utilisateur();
-        $form = $this->createForm(Utilisateur1Type::class, $utilisateur);
+        $form = $this->createForm(UtilisateurType::class, $utilisateur);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -53,7 +53,7 @@ final class AdminUtilisateurController extends AbstractController
     #[Route('/{id}/edit', name: 'app_admin_utilisateur_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Utilisateur $utilisateur, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(Utilisateur1Type::class, $utilisateur);
+        $form = $this->createForm(UtilisateurType::class, $utilisateur);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
