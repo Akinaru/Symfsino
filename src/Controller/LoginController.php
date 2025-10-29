23d6a6f51972aec1b7e8a787c0086141d0643be7
Akinaru <maxime.utchimata@gmail.com>
2025-10-29 11:55:36 +0100

    UPD [login] Connexion

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,16 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-final class LoginController extends AbstractController
+class LoginController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(
-        AuthenticationUtils $authenticationUtils
-    ): Response {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('app_front_home');
-        }
-
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
@@ -29,10 +24,9 @@ final class LoginController extends AbstractController
         ]);
     }
 
-    #[Route(path: '/logout', name: 'app_front_logout')]
+    #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
-
 }
